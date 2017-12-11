2b0ae457fd97a5800fc59b722c62f526d7a3163a
Shanjiadong <1312282489@qq.com>
2017-12-11 12:59:57 +0800

    Initial commit

---
+++ Test_githubActivity.java
@@ -0,0 +1,13 @@
+package com.example.john.test_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test_githubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_github);
+    }
+}
